c75f99d29870b37584717d12d6ab5cbaf52ca343
captainzonks <zonks.zonks@gmail.com>
2020-03-29 10:24:35 -0600

    game menu functionality being implemented

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour
 
     public CharStats[] playerStats;
 
+    public bool gameMenuOpen, dialogActive, fadingBetweenAreas;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,13 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(gameMenuOpen || dialogActive || fadingBetweenAreas)
+        {
+            PlayerController.instance.canMove = false;
+        }
+        else
+        {
+            PlayerController.instance.canMove = true;
+        }
     }
 }
